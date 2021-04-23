0e19acc60ecbaeb4b3f6c6ffc5c936d9a6836d59
Chelo-Mar <82545548+Chelo-Mar@users.noreply.github.com>
2021-04-22 23:01:58 -0500

    Add files via upload

---
+++ calculador.java
@@ -0,0 +1,22 @@
+
+public class calculador 
+{
+    double cuadrado ;
+    int indicador;
+    double punto;
+    int ubicador2;
+   
+      double Cuadrado(double a){
+       double Base=0.0;
+       double altura = 0.0;
+       return cuadrado;
+    }
+       public double areaCuadrado(double lado){
+       double area=0.0;
+       area = lado*lado;
+       return area;
+    
+    }
+    }
+    
+
